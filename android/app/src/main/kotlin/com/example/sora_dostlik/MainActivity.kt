fe384e0d4981e9b958b5a0bca0d2af070da36b83
Murodjon <yoshxaker004@gmail.com>
2025-09-02 10:31:00 +0500

    Tolq bitgan versiya

---
+++ android/app/src/main/kotlin/com/example/sora_dostlik/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sora_dostlik
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Global/Api_global.dart
@@ -0,0 +1,4 @@
+class ApiConfig {
+static const String baseUrl = "https://soradoslik.richman.uz/api";
+
+}
